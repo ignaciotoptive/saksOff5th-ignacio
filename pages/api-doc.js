d109e55f1fb06a473b83010a8742caad39919185
ignacio <ignacio@toptive.co>
2022-08-19 12:58:18 -0300

    setup API doc

---
+++ pages/api-doc.js
@@ -0,0 +1,29 @@
+import { createSwaggerSpec } from 'next-swagger-doc';
+import dynamic from 'next/dynamic';
+import 'swagger-ui-react/swagger-ui.css';
+
+const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
+
+function ApiDoc({ spec }) {
+  return <SwaggerUI spec={spec} />;
+}
+
+export const getStaticProps = async () => {
+  const spec = createSwaggerSpec({
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Webstore API Doc',
+        version: '1.0',
+      },
+    },
+  });
+
+  return {
+    props: {
+      spec,
+    },
+  };
+};
+
+export default ApiDoc;
